6d4509b8654ba0c5069c38651493eeb16ea5e0ca
Mir Hussain <mirhussainjan10387@gmail.com>
2024-12-01 13:25:32 +0500

    Minor Changes in UI to make consistency

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,21 @@ import RecentExpenses from "../components/RecentExpenses"; // Correct path for G
 import PersonalInformation from "../components/PersonalInformation";
 import CardSlider from "../components/CardSlider";
 import Nav from "../components/Nav";
+import CardSwiper from "../components/CardSwiper";
 
 
 const Home = () => {
   return (
     <div className="p-4 min-h-screen">
       <div className="row my-5 mx-0 gap-4 justify-between">
-      <CardSlider />
+      <CardSwiper/>
       <StatCards />
       </div>
       <div className="row mx-0 my-4 justify-between">
       <PersonalInformation/>
       <RecentExpenses />
       </div>
-      <Nav /> {/* Nav component */}
+      <Nav />
     </div>
   );
 };
